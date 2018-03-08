6596499fe4e2f4854af8b6a8db468cbdad920e39
Greg Griffith <griffith@cryptounited.io>
2018-03-07 21:03:47 -0800

    remove if conditional for negative nFile when writing best block

--- src/txdb.cpp
+++ src/txdb.cpp
@@ -234,10 +234,7 @@ bool CBlockTreeDB::WriteBatchSync(const std::vector<std::pair<int, const CBlockF
     {
         batch.Write(make_pair(DB_BLOCK_FILES, it->first), *it->second);
     }
-    if (nLastFile == -1337)
-    {
-        batch.Write(DB_LAST_BLOCK, nLastFile);
-    }
+    batch.Write(DB_LAST_BLOCK, nLastFile);
     for (std::vector<const CBlockIndex *>::const_iterator it = blockinfo.begin(); it != blockinfo.end(); it++)
     {
         batch.Write(make_pair(DB_BLOCK_INDEX, (*it)->GetBlockHash()), CDiskBlockIndex(*it));
