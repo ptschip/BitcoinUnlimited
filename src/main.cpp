a06a39cac867c6b3e622636a8ae76a5d27953f0e
Greg Griffith <griffith@cryptounited.io>
2018-06-15 14:45:00 -0400

    remove writebestblockdb as view now handles it

--- src/main.cpp
+++ src/main.cpp
@@ -1504,12 +1504,6 @@ static DisconnectResult DisconnectBlock(const CBlock &block, const CBlockIndex *
     // move best block pointer to prevout block
     view.SetBestBlock(pindex->pprev->GetBlockHash());
 
-    // if we are not set to sequential, update the best block hash for leveldb storage
-    if(BLOCK_DB_MODE != SEQUENTIAL_BLOCK_FILES)
-    {
-        pcoinsdbview->WriteBestBlockDb(pindex->pprev->GetBlockHash());
-    }
-
     return fClean ? DISCONNECT_OK : DISCONNECT_UNCLEAN;
 }
 
@@ -1726,12 +1720,6 @@ bool ConnectBlock(const CBlock &block,
         if (!fJustCheck)
         {
             view.SetBestBlock(pindex->GetBlockHash());
-
-            // if we are not set to sequential, update the best block hash for leveldb storage
-            if(BLOCK_DB_MODE != SEQUENTIAL_BLOCK_FILES)
-            {
-                pcoinsdbview->WriteBestBlockDb(pindex->GetBlockHash());
-            }
         }
         return true;
     }
@@ -2084,12 +2072,6 @@ bool ConnectBlock(const CBlock &block,
 
     // add this block to the view's block chain (the main UTXO in memory cache)
     view.SetBestBlock(pindex->GetBlockHash());
-
-    // if we are not set to sequential, update the best block hash for leveldb storage
-    if(BLOCK_DB_MODE != SEQUENTIAL_BLOCK_FILES)
-    {
-        pcoinsdbview->WriteBestBlockDb(pindex->GetBlockHash());
-    }
 
     int64_t nTime5 = GetTimeMicros();
     nTimeIndex += nTime5 - nTime4;
