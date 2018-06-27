9cbbca8b77724dd2b3f821fd3f12b8be2d283d9b
Greg Griffith <griffith@cryptounited.io>
2018-06-27 01:54:56 -0400

    add readblock and readundo
    make dbwrapper data members public so we can access them for readblock and readundo

--- src/blockdb/blockdb.h
+++ src/blockdb/blockdb.h
@@ -102,6 +102,71 @@ private:
 
 public:
     bool WriteBatchSync(const std::vector<CBlock> &blocks);
+
+    // we need a custom read functions to account for the way we want to deserialize blockdbvalue and undodbvalue
+    template <typename K>
+    bool ReadBlock(const K &key, BlockDBValue &value, CBlock& block) const
+    {
+        CDataStream ssKey(SER_DISK, CLIENT_VERSION);
+        ssKey.reserve(DBWRAPPER_PREALLOC_KEY_SIZE);
+        ssKey << key;
+        leveldb::Slice slKey(ssKey.data(), ssKey.size());
+
+        std::string strValue;
+        leveldb::Status status = pdb->Get(readoptions, slKey, &strValue);
+        if (!status.ok())
+        {
+            if (status.IsNotFound())
+                return false;
+            LOGA("LevelDB read failure: %s\n", status.ToString());
+            dbwrapper_private::HandleError(status);
+        }
+        try
+        {
+            CDataStream ssValue(strValue.data(), strValue.data() + strValue.size(), SER_DISK, CLIENT_VERSION);
+            ssValue.Xor(obfuscate_key);
+            ssValue.readtovoid(&value.nVersion, sizeof(int32_t));
+            ssValue.readtovoid(&value.blockHeight, sizeof(uint64_t));
+            ssValue.readtovoid(&block, ssValue.size());
+        }
+        catch (const std::exception &)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    template <typename K>
+    bool ReadUndo(const K &key, UndoDBValue &value, CBlockUndo& blockundo) const
+    {
+        CDataStream ssKey(SER_DISK, CLIENT_VERSION);
+        ssKey.reserve(DBWRAPPER_PREALLOC_KEY_SIZE);
+        ssKey << key;
+        leveldb::Slice slKey(ssKey.data(), ssKey.size());
+
+        std::string strValue;
+        leveldb::Status status = pdb->Get(readoptions, slKey, &strValue);
+        if (!status.ok())
+        {
+            if (status.IsNotFound())
+                return false;
+            LOGA("LevelDB read failure: %s\n", status.ToString());
+            dbwrapper_private::HandleError(status);
+        }
+        try
+        {
+            CDataStream ssValue(strValue.data(), strValue.data() + strValue.size(), SER_DISK, CLIENT_VERSION);
+            ssValue.Xor(obfuscate_key);
+            ssValue.readtovoid(&value.hashChecksum, 32); // 32 is number of bytes in uint256
+            ssValue.readtovoid(&value.hashBlock, 32); // 32 is number of bytes in uint256
+            ssValue.readtovoid(&blockundo, ssValue.size());
+        }
+        catch (const std::exception &)
+        {
+            return false;
+        }
+        return true;
+    }
 };
 
 extern CBlockDB *pblockdb;
